d763902b02fe3ed5e4dac904fe8c34241c46339e
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-03 14:58:16 -0300

    📦 refactor: working with abstraction.

--- src/app/components/screen/screen.component.ts
+++ src/app/components/screen/screen.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, OnInit } from '@angular/core';
+import { MATH } from 'src/app/math.token';
+import { MathCount } from '../../math.interface';
 
 @Component({
   selector: 'app-screen',
   templateUrl: './screen.component.html',
-  styleUrls: ['./screen.component.scss']
+  styleUrls: ['./screen.component.scss'],
 })
 export class ScreenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @ContentChild(MATH, { static: true }) math: MathCount;
 
   ngOnInit(): void {
+    this.math.load();
   }
 
+  refresh(): void {
+    this.math.refresh();
+  }
 }
